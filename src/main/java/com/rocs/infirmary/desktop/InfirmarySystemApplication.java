a16b5c679b850f7aa7bd3cf47a9cd1322ee846f1
jakeeviado <jakematthewviado204@gmail.com>
2025-04-08 22:12:53 +0800

    fix! menu service

--- src/main/java/com/rocs/infirmary/desktop/InfirmarySystemApplication.java
+++ src/main/java/com/rocs/infirmary/desktop/InfirmarySystemApplication.java
@@ -212,8 +212,8 @@ public class InfirmarySystemApplication {
 
                 for (Student record : medicalRecords) {
                     System.out.println();
-                    System.out.println("Firstname             : " + record.getFirstName());
-                    System.out.println("Middlename            : " + record.getMiddleName());
+                    System.out.println("First name             : " + record.getFirstName());
+                    System.out.println("Middle name            : " + record.getMiddleName());
                     System.out.println("Lastname              : " + record.getLastName());
                     System.out.println("Age                   : " + record.getAge());
                     System.out.println("Gender                : " + record.getGender());
